46288e9766461b8d4d30a0510349941f5e63d748
daupa001xamk <daupa001@edu.xamk.fi>
2023-09-24 15:43:46 +0300

    github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://daupa001xamk.github.io',
 });
